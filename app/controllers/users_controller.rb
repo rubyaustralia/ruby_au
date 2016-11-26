dfb3fb56a0ffe3bbbfe997b47fcf3e4ac2514891
Edward Tippett <edward.tippett@gmail.com>
2016-11-26 14:17:27 +1030

    Add Member info to member sign up flow
    - Create new JoiningMember form and update specs.
    - Ask new members for some basic details for records.
    - Allow members to opt in for displaying their profile publicly.
    - Increase code coverage for sign up paths.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < Clearance::UsersController
+  def new
+    @joining_member = JoiningMember.new(user: User.new)
+    render "users/new"
+  end
+
+  def create
+    @joining_member = JoiningMember.new(user: User.new)
+    @joining_member.attributes = form_params
+
+    if @joining_member.save
+      sign_in @joining_member.user
+      redirect_back_or url_after_create
+    else
+      render "users/new"
+    end
+  end
+
+  private
+
+  def form_params
+    params.require(:joining_member).permit!.to_h
+  end
+end
