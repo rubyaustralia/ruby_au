fe7eb69473518af774257e41c4d0d9ebe31c5c14
David Looi <david@cultureamp.com>
2018-03-21 10:41:43 +1100

    Remove unused code

--- spec/support/controller_helper.rb
+++ spec/support/controller_helper.rb
@@ -1,11 +1,6 @@
 module ControllerHelpers
   def sign_in(user)
-    if user.nil?
-      allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, scope: :user)
-      allow(controller).to receive(:current_user).and_return(nil)
-    else
-      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
-      allow(controller).to receive(:current_user).and_return(user)
-    end
+    allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+    allow(controller).to receive(:current_user).and_return(user)
   end
 end
