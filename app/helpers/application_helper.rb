04f6e1a753f3b9bb85b3ed753a2d2c5cb8e1ce0d
Pat Allan <pat@freelancing-gods.com>
2019-05-11 14:15:35 +0800

    Rework home page copy.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def link_to_external(name = nil, options = nil, html_options = {}, &block)
+    svg = inline_svg "external-link.svg", height: 12
+    html_options[:target] ||= "_blank"
+    html_options[:rel]    ||= "external"
+
+    if block_given?
+      link_to capture(&block).strip.html_safe + svg, options, html_options
+    else
+      link_to (name + svg).html_safe, options, html_options
+    end
+  end
 end
