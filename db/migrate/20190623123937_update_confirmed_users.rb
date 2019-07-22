1b7b8d93507ef9dbbcc8296685294fc4d24c0cba
Pat Allan <pat@freelancing-gods.com>
2019-07-22 11:24:08 +1000

    Avoid validations for this update.

--- db/migrate/20190623123937_update_confirmed_users.rb
+++ db/migrate/20190623123937_update_confirmed_users.rb
@@ -1,5 +1,5 @@
 class UpdateConfirmedUsers < ActiveRecord::Migration[5.2]
   def up
-    User.where(email_confirmed: true).update(confirmed_at: Time.current)
+    User.where(email_confirmed: true).update_all(confirmed_at: Time.current)
   end
 end
