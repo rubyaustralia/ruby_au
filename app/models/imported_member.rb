b10a226b0065afffd6f2191b380b14254a7b2221
Pat Allan <pat@freelancing-gods.com>
2019-07-24 15:21:30 +1000

    Add unique tokens for imported members.

--- app/models/imported_member.rb
+++ app/models/imported_member.rb
@@ -1,3 +1,15 @@
 class ImportedMember < ApplicationRecord
   scope :uncontacted, -> { where(contacted_at: nil) }
+
+  validates :full_name, presence: true
+  validates :email, presence: true
+  validates :token, presence: true, uniqueness: true
+
+  before_validation :set_token, on: :create
+
+  private
+
+  def set_token
+    self.token = SecureRandom.uuid
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_051647) do
+ActiveRecord::Schema.define(version: 2019_07_22_075252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_07_22_051647) do
     t.datetime "contacted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token", null: false
     t.index ["email"], name: "index_imported_members_on_email"
   end
 
