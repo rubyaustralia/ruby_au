574a3635c6b7cd0ec4d9106bb81486e310ec09dc
Pat Allan <pat@freelancing-gods.com>
2019-07-30 10:25:16 +1000

    Allow committee to add/edit access requests.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :memberships, only: [:index]
+    resources :access_requests, except: [:destroy]
   end
 
   get '/forum', to: redirect('https://forum.ruby.org.au'),
--- spec/features/committee_manages_members_spec.rb
+++ spec/features/committee_manages_members_spec.rb
@@ -26,4 +26,26 @@ RSpec.describe "Committee managing members", type: :feature do
     expect(page).to have_content "Alex"
     expect(page).to_not have_content "Riley"
   end
+
+  scenario "adds a new access request" do
+    visit root_path
+    click_link "Access Requests"
+    click_link "Add"
+
+    fill_in "Name", with: "Alex"
+    fill_in "Reason", with: "Contacting members"
+    click_button "Save"
+
+    expect(page).to have_content("Alex")
+    expect(page).to have_content("Not yet")
+
+    click_link "Edit"
+    select "2019", from: "access_request_viewed_on_1i"
+    select "December", from: "access_request_viewed_on_2i"
+    select "31", from: "access_request_viewed_on_3i"
+    click_button "Save"
+
+    expect(page).to have_content("Alex")
+    expect(page).to have_content("2019-12-31")
+  end
 end
