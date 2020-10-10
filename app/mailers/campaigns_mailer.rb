0942be03e7abe257ec4a314a5c53283a607ebbf5
Pat Allan <pat@freelancing-gods.com>
2020-10-10 12:08:32 +1100

    Add event attachment to campaign emails.
    Defaulting to events of 90 minutes long, because we don’t currently track finishing times. It’ll do for now.

--- app/mailers/campaigns_mailer.rb
+++ app/mailers/campaigns_mailer.rb
@@ -10,6 +10,10 @@ class CampaignsMailer < ApplicationMailer
 
     prepare_event_variables campaign
 
+    if campaign.rsvp_event.present?
+      attachments['event.ics'] = calendar(campaign.rsvp_event)
+    end
+
     mail(
       to: @membership.user.email,
       subject: campaign.subject
@@ -17,6 +21,23 @@ class CampaignsMailer < ApplicationMailer
   end
 
   private
+
+  def calendar(rsvp_event)
+    calendar = Icalendar::Calendar.new
+
+    calendar.event do |event|
+      event.dtstart = rsvp_event.happens_at
+      event.dtend   = rsvp_event.happens_at + 90.minutes
+      event.summary = rsvp_event.title
+
+      event.alarm do |alarm|
+        alarm.summary = "#{rsvp_event.title} Reminder"
+        alarm.trigger = "-P0DT1H0M0S" # 1 hour before
+      end
+    end
+
+    calendar.to_ical
+  end
 
   def prepare_event_variables(campaign)
     return if campaign.rsvp_event.blank?
