86802e54db7d4bd0fb0e87701388097ffba3ddbb
Mathew Button <mathew@thelookoutway.com>
2024-11-01 21:52:40 +1300

    Allow slack invite ID to be configured from ENV
    So that when the link runs out of uses, it can be replaced with another
    ID without a code deployment.

--- app/controllers/my/slack_invites_controller.rb
+++ app/controllers/my/slack_invites_controller.rb
@@ -1,7 +1,7 @@
 module My
   class SlackInvitesController < My::ApplicationController
     def show
-      redirect_to "https://join.slack.com/t/rubyau/shared_invite/zt-1pewt4vi8-TtrM~UoIJmuH9Niy0Ela6w", allow_other_host: true
+      redirect_to "https://join.slack.com/t/rubyau/shared_invite/#{ENV['SLACK_INVITE_ID']}", allow_other_host: true
     end
   end
 end
