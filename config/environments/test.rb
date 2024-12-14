3da6d84548282bad264c7ee0b3f6679824858d54
Lee Sheppard <lee@leesheppard.com>
2024-12-14 21:26:25 +1000

    Task/upgrade to rails 8 (#265)
    * Upgrade 7.2 to 8.0

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,4 @@
 require "active_support/core_ext/integer/time"
-
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
 # your test database is "scratch space" for the test suite and is wiped
@@ -25,9 +24,8 @@ Rails.application.configure do
     "Cache-Control" => "public, max-age=#{1.hour.to_i}"
   }
 
-  # Show full error reports and disable caching.
+  # Show full error reports.
   config.consider_all_requests_local = true
-  config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
   # Render exception templates for rescuable exceptions and raise for other exceptions.
@@ -36,18 +34,15 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
-  config.action_mailer.perform_caching = false
-
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # config/environments/{development,test}.rb
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.perform_caching = false
 
-  # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  # Set host to be used by links generated in mailer templates.
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
@@ -55,12 +50,15 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
+  # Print deprecation notices to the stderr.
+  config.active_support.deprecation = :stderr
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  # Raise error when a before_action's only/except options reference missing actions
+  # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 end
