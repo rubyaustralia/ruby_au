a41adaa3574f1062a6ee7d688fd0af4c8a726d3c
Ken Ding <ken.ding@mail.com>
2025-01-14 00:12:46 +1100

    Add slack link to confirmation email

---
+++ spec/views/devise/mailer/confirmation_instructions.html.erb_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "devise/mailer/confirmation_instructions.html.erb", type: :view do
+  include Rails.application.routes.url_helpers
+
+  let(:user) { create(:user, full_name: "John Doe") }
+  let(:token) { "fake_token" }
+
+  before do
+    assign(:resource, user)
+    assign(:token, token)
+
+    render
+  end
+
+  it "displays the preheader text" do
+    expect(rendered).to include("Confirm my membership")
+  end
+
+  it "displays the welcome message with the user's full name" do
+    expect(rendered).to match(/Welcome John Doe!/)
+  end
+
+  it "displays the confirmation instructions" do
+    expect(rendered).to match(/To confirm both your email address and your membership for Ruby Australia, please follow the link below:/)
+  end
+
+  it "displays the confirmation link" do
+    expect(rendered).to have_link('Confirm my membership', href: confirmation_url(user, confirmation_token: token))
+  end
+
+  it "displays the Slack community invitation" do
+    expect(rendered).to have_link('Slack', href: slack_url)
+  end
+end
