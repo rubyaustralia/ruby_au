5e9af17a464865517a1ef7d93a64c7d7f7e21b3c
Lee Sheppard <lee@leesheppard.com>
2025-02-14 21:45:57 +1100

    Refactor - rubocop being rubocop

--- app/lib/mailing_list/setup.rb
+++ app/lib/mailing_list/setup.rb
@@ -14,9 +14,7 @@ class MailingList::Setup
 
   def call
     user.mailing_lists_will_change!
-
     user.mailing_lists[list.name] = subscribed? ? "true" : "false"
-
     user.save
   end
 
@@ -29,25 +27,32 @@ class MailingList::Setup
   end
 
   def subscribed?
-    return true if Rails.env.development? && ENV['CAMPAIGN_MONITOR_API_KEY'].blank?
+    return true if development_without_api_key?
 
-    CreateSend::Subscriber.get(
-      cm_auth,
-      list.api_id,
-      user.email
-    )["State"] == "Active"
-  rescue CreateSend::BadRequest => br
-    Rails.logger.error "Campaign Monitor Bad Request: #{br}"
-    Rails.logger.error "Error Code: #{br.data.Code}"
-    Rails.logger.error "Error Message: #{br.data.Message}"
+    response = fetch_subscription_status
+    handle_subscription_response(response)
+  rescue CreateSend::BadRequest, CreateSend::Unauthorized, StandardError => e
+    log_campaign_monitor_error(e)
     false
-  rescue CreateSend::Unauthorized => ua
-    Rails.logger.error "Campaign Monitor Unauthorized: #{ua}"
-    Rails.logger.error "Error Code: #{ua.data.Code}"
-    Rails.logger.error "Error Message: #{ua.data.Message}"
-    false
-  rescue StandardError => e
-    Rails.logger.error "Campaign Monitor Error: #{e}"
-    false
+  end
+
+  def development_without_api_key?
+    Rails.env.development? && ENV['CAMPAIGN_MONITOR_API_KEY'].blank?
+  end
+
+  def fetch_subscription_status
+    CreateSend::Subscriber.get(cm_auth, list.api_id, user.email)
+  end
+
+  def handle_subscription_response(response)
+    response && response["State"] == "Active"
+  end
+
+  def log_campaign_monitor_error(error)
+    Rails.logger.error "Campaign Monitor Error: #{error}"
+    return unless error.respond_to?(:data)
+
+    Rails.logger.error "Error Code: #{error.data.Code}" if error.data.respond_to?(:Code)
+    Rails.logger.error "Error Message: #{error.data.Message}" if error.data.respond_to?(:Message)
   end
 end
