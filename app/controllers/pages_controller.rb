acb3f5231833af1db7d60f66a425a82ed6098643
Lee Sheppard <lee@leesheppard.com>
2025-03-13 22:24:50 +1100

    Rubocop corrections for exception

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,15 +12,13 @@ class PagesController < ApplicationController
   end
 
   def show
-    if params[:id] == 'welcome'
-      @posts = Post.published_with_associations.order(published_at: :desc).limit(5)
-    end
+    @posts = Post.published_with_associations.order(published_at: :desc).limit(5) if params[:id] == 'welcome'
     render "pages/#{params[:id]}"
-  rescue ActionView::MissingTemplate => exception
-    if exception.message.match?(/Missing template pages#{request.path}/)
+  rescue ActionView::MissingTemplate => e
+    if e.message.match?(/Missing template pages#{request.path}/)
       raise ActionController::RoutingError, "No such page: #{params[:id]}"
     else
-      raise exception
+      raise e
     end
   end
 end
