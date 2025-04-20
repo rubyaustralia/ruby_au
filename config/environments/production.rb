28752e3d0a5d21aebb5a59bba562bade0b62633a
Lee Sheppard <lee@leesheppard.com>
2025-04-20 23:24:25 +1000

    Remove assets pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # config.assets.compile = true
 
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
