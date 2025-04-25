5ada7b3bf3845e575f621bb94bd834afec1fcd59
Lee Sheppard <lee@leesheppard.com>
2025-04-25 22:45:22 +1000

    Remove theme for version 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,9 @@ module.exports = {
     './app/frontend/**/*.{js,ts,css,scss}',
     './app/components/**/*.{html.erb,rb}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        pearl: '#F0F0F0',
-        red: '#D30001',
-        'red-dark': '#900000',
-        'red-darker': '#700000',
-      },
-    },
-  },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography'),
   ],
 };
