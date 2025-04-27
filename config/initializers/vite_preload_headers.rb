deda208e6ef863cc74c7277042faf0a6b772213f
Lee Sheppard <lee@leesheppard.com>
2025-04-27 17:39:31 +1000

    Configure compression for JS CSS

--- config/initializers/vite_preload_headers.rb
+++ config/initializers/vite_preload_headers.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before(ActionDispatch::Static, Rack::Static, {
-                                                    urls: %w[/vite-dev /vite-production],
-                                                    header_rules: [
-                                                      [:all, { 'Cache-Control' => 'public, max-age=31536000' }],
-                                                      [%w(css js), { 'Content-Type' => 'text/css; charset=utf-8' }],
-                                                      [%w(js), { 'Content-Type' => 'text/javascript; charset=utf-8' }]
-                                                    ]
-                                                  })
+  urls: %w[/vite-dev /vite-production],
+  header_rules: [
+    [:all, {'Cache-Control' => 'public, max-age=31536000'}],
+    [%w(css js), {'Content-Type' => 'text/css; charset=utf-8'}],
+    [%w(js), {'Content-Type' => 'text/javascript; charset=utf-8'}]
+  ]
+})
