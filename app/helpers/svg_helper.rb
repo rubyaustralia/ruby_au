bbcd39b4d4efcb8adf28f4dd0cd789ba019430a1
Lee Sheppard <lee@leesheppard.com>
2025-05-02 23:33:46 +1000

    Sanitize svg

--- app/helpers/svg_helper.rb
+++ app/helpers/svg_helper.rb
@@ -2,15 +2,30 @@ module SvgHelper
   def vite_inline_svg(name, options = {})
     svg_file_path = Rails.root.join('app', 'frontend', 'images', name.to_s)
 
-    if File.exist?(svg_file_path)
-      svg_content = File.read(svg_file_path)
+    return "SVG not found: #{name}" unless File.exist?(svg_file_path)
 
-      attributes = options.map { |k, v| "#{k}=\"#{v}\"" }.join(' ')
-      svg_content.sub!(/^<svg/, "<svg #{attributes}")
+    svg_content = File.read(svg_file_path)
+    process_svg_content(svg_content, options)
+  end
 
-      raw(svg_content)
-    else
-      "SVG not found: #{name}"
-    end
+  private
+
+  def process_svg_content(svg_content, options)
+    doc = Nokogiri::HTML::DocumentFragment.parse(svg_content)
+    svg = doc.at_css('svg')
+
+    apply_options_to_svg(svg, options) if svg
+
+    sanitize_svg(doc)
+  end
+
+  def apply_options_to_svg(svg, options)
+    options.each { |key, value| svg[key.to_s] = value }
+  end
+
+  def sanitize_svg(doc)
+    sanitize(doc.to_html,
+             tags: %w[svg path g circle rect line polyline polygon use defs clipPath title desc],
+             attributes: %w[id class style viewBox fill stroke stroke-width stroke-linecap stroke-linejoin width height x y cx cy r transform d points xmlns xlink:href clip-path])
   end
 end
