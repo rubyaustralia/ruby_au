6e10b3aefca4b850fac4316d0abff7b9aab2048a
Lee Sheppard <lee@leesheppard.com>
2025-05-05 22:58:29 +1000

    Create missing configurations for Active Storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,9 +124,9 @@ Rails.application.configure do
   config.active_storage.default_url_options = { host: 'ruby.org.au', protocol: 'https' }
 
   config.middleware.use Rack::Deflater
-
   config.public_file_server.headers = {
     'Cache-Control' => 'public, max-age=31536000',
     'Expires' => 1.year.from_now.to_formatted_s(:rfc822)
   }
+  config.active_storage.resolve_model_to_route = :rails_storage_redirect
 end
