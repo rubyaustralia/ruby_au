bc96e0dce4a9c7d42242dfa475d0e9b0496a937b
Lee Sheppard <lee@leesheppard.com>
2025-06-15 22:57:36 +1000

    Raise mailer errors set to true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.cache_store = :memory_store
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
