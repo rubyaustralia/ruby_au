380baa2a02779d688c7548cdf14012ca1531b132
Julián Pinzón Eslava <julian@julianpinzon.com>
2025-07-31 00:03:12 +1000

    Add index page and fix responsive issues

--- sites/melbourne/app/controllers/melbourne/events_controller.rb
+++ sites/melbourne/app/controllers/melbourne/events_controller.rb
@@ -2,6 +2,10 @@
 
 module Melbourne
   class EventsController < ApplicationController
+    def index
+      @events = Event.all.reverse
+    end
+
     def show
       event = Event.find_by_slug(params[:slug]) # rubocop:disable Rails/DynamicFindBy
       @event_presenter = EventPresenter.new(event)
