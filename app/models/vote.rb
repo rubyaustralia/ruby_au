aa42cffea755aa1a303eb219aeb348856c837516
Timothy Reen <reentim@gmail.com>
2025-10-08 14:43:03 +1100

    Sketch out potential way to model scored elections

---
+++ app/models/vote.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id            :bigint           not null, primary key
+#  score         :integer          not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  nomination_id :bigint           not null
+#  voter_id      :bigint           not null
+#
+# Indexes
+#
+#  index_votes_on_nomination_id               (nomination_id)
+#  index_votes_on_voter_id                    (voter_id)
+#  index_votes_on_voter_id_and_nomination_id  (voter_id,nomination_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (voter_id => users.id)
+#
+class Vote < ApplicationRecord
+  belongs_to :nomination
+  belongs_to :voter, class_name: 'User'
+end
