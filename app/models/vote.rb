f5bf24e55a79cc726a3047a67f0c26d3455e8c69
Timothy Reen <reentim@gmail.com>
2025-11-24 15:21:10 +1100

    Votes belong to polymorphic votable association

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,25 +2,26 @@
 #
 # Table name: votes
 #
-#  id            :bigint           not null, primary key
-#  score         :integer          not null
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  nomination_id :bigint           not null
-#  voter_id      :bigint           not null
+#  id           :bigint           not null, primary key
+#  score        :integer          not null
+#  votable_type :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  votable_id   :bigint           not null
+#  voter_id     :bigint           not null
 #
 # Indexes
 #
-#  index_votes_on_nomination_id               (nomination_id)
-#  index_votes_on_voter_id                    (voter_id)
-#  index_votes_on_voter_id_and_nomination_id  (voter_id,nomination_id) UNIQUE
+#  index_votes_on_votable                  (votable_type,votable_id)
+#  index_votes_on_voter_id                 (voter_id)
+#  index_votes_on_voter_id_and_votable_id  (voter_id,votable_id) UNIQUE
 #
 # Foreign Keys
 #
 #  fk_rails_...  (voter_id => users.id)
 #
 class Vote < ApplicationRecord
-  belongs_to :nomination
+  belongs_to :votable, polymorphic: true
   belongs_to :voter, class_name: 'User'
 
   validate :election_must_be_open_validation
@@ -28,6 +29,6 @@ class Vote < ApplicationRecord
   private
 
   def election_must_be_open_validation
-    errors.add(:base, "Election is not currently open") unless nomination.election.open?
+    errors.add(:base, "Election is not currently open") if votable.is_a?(Election) && !votable.election.open?
   end
 end
