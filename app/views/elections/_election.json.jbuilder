d500c45088fb031f2363d6efe7f7a2f6e7abd00c
Timothy Reen <reentim@gmail.com>
2025-11-25 01:37:10 +1100

    Create election views and controllers

---
+++ app/views/elections/_election.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! election, :id, :created_at, :updated_at
+json.url election_url(election, format: :json)
--- db/migrate/20251007055656_create_elections.rb
+++ db/migrate/20251007055656_create_elections.rb
@@ -1,9 +1,10 @@
 class CreateElections < ActiveRecord::Migration[8.0]
   def change
     create_table :elections do |t|
-      t.string :position, null: false
+      t.string :title, null: false
       t.integer :vacancies, default: 1, null: false
-      t.integer :point_scale, default: 10, null: false
+      t.integer :maximum_score, default: 5, null: false
+      t.integer :minimum_score, default: -5, null: false
       t.datetime :opened_at
       t.datetime :closed_at
 
