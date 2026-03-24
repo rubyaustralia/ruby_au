3f401da73134c4502b833f5189a736f219ba48a5
Lee Sheppard <lee@leesheppard.com>
2026-03-24 13:00:28 +1100

    Fix warning Regular expression injection
    The regex is built from a user-controlled value, so it can behave unpredictably or be abused. Avoiding the use of Regex to resolve.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,7 +15,7 @@ class PagesController < ApplicationController
     @posts = Post.published_with_associations.order(published_at: :desc).limit(5) if valid_page == 'welcome'
     render "pages/#{valid_page}"
   rescue ActionView::MissingTemplate => e
-    if e.message.match?(/Missing template pages#{request.path}/)
+    if e.message.include?("Missing template pages#{request.path}")
       raise ActionController::RoutingError, "No such page: #{params[:id]}"
     else
       raise e
