43f385de796e1575e524780912b9309fa4f911fd
Adam Rice <development@hashnotadam.com>
2026-03-26 09:40:50 +1100

    Melbourne: Redirect /talks to GitHub

--- sites/melbourne/config/routes/melbourne.rb
+++ sites/melbourne/config/routes/melbourne.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
       get "/", to: "home#show", as: :home
 
       resources :events, only: %i[index show], param: :slug
+
+      get "/talks", to: redirect(
+        "https://github.com/rubyaustralia/melbourne-ruby/issues", status: 302
+      )
     end
   end
 end
