8e1017a75052c679e591336f43fe52b6f388c2f7
Audrey Barangan <audreeedale@gmail.com>
2026-05-03 22:01:52 +1000

    add test

---
+++ spec/mailers/campaigns_mailer_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe CampaignsMailer, type: :mailer do
+  describe '#campaign_email' do
+    it "sends to the user's primary email" do
+      user = create(:user, email: 'old@example.com')
+      create(:email, user: user, email: 'primary@example.com', primary: true)
+
+      membership = create(:membership, user: user)
+      campaign = create(:campaign, subject: 'Test campaign')
+
+      mail = described_class.campaign_email(campaign, membership, nil).deliver_now
+
+      expect(mail.to).to eq(['primary@example.com'])
+    end
+
+    it "falls back to the user's default email if no primary email exists" do
+      user = create(:user, email: 'fallback@example.com')
+
+      membership = create(:membership, user: user)
+      campaign = create(:campaign, subject: 'Test campaign')
+
+      mail = described_class.campaign_email(campaign, membership, nil).deliver_now
+
+      expect(mail.to).to eq(['fallback@example.com'])
+    end
+  end
+end
