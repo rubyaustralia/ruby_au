bb9f81ea33cd2ab9550699ceaefa41e70928b616
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2026-05-27 18:10:58 +1000

    fix controller tests event mocking issue

--- sites/melbourne/spec/requests/upcoming_events_spec.rb
+++ sites/melbourne/spec/requests/upcoming_events_spec.rb
@@ -6,7 +6,12 @@ RSpec.describe Melbourne::UpcomingEventsController, type: :request do
   end
 
   describe "index" do
-    let(:events) { Melbourne::Event.upcoming }
+    let(:events) do
+      [
+        Melbourne::Event.new(name: "test event-1", date: Time.zone.today, slug: "test-1 for controller"),
+        Melbourne::Event.new(name: "test event-2", date: Time.zone.today + 2, slug: "test-2 for controller")
+      ]
+    end
 
     before do
       allow(Melbourne::Event).to receive(:upcoming).and_return(events)
@@ -18,9 +23,10 @@ RSpec.describe Melbourne::UpcomingEventsController, type: :request do
     end
 
     it "renders the index view" do
-      event = events.first
       get melbourne_upcoming_events_path
-      expect(response.body).to include(event.name)
+      events.each do |event|
+        expect(response.body).to include(event.name)
+      end
     end
   end
 end
