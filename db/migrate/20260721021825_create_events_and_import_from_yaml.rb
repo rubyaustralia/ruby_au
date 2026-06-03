10ad4eaa2c2b34f72c1ddd054c4e6702dfb0d676
Kirsten Comandich <kcomandich@gmail.com>
2026-06-02 22:15:53 -0700

    Import event data from the yaml files into new database tables, in db migration

--- db/migrate/20260721021825_create_events_and_import_from_yaml.rb
+++ db/migrate/20260721021825_create_events_and_import_from_yaml.rb
@@ -1,3 +1,9 @@
+DATABASE_EVENT_DEFAULTS = {
+  region: 'melbourne',
+  start_time: '6:30pm',
+  end_time: '8:30pm'
+}.freeze
+
 class CreateEventsAndImportFromYaml < ActiveRecord::Migration[8.1]
   def change
 
@@ -34,6 +40,74 @@ class CreateEventsAndImportFromYaml < ActiveRecord::Migration[8.1]
 
     change_table :users do |t|
       t.boolean :meetup_admin, default: false, null: false
+    end
+
+    reversible do |direction|
+      direction.up do
+        import_yaml_events
+        set_seeds_meetup_admin
+      end
+    end
+  end
+
+  private
+
+  def format_address(address)
+    "#{address.street} #{address.locality} #{address.postal_code} #{address.region} #{address.country}"
+  end
+
+  def build_db_venue(event)
+    Venue.find_or_create_by!(
+      **event.venue.slice(:name, :google_maps_url),
+      address: format_address(event.venue.address)
+    )
+  end
+
+  def build_db_talks(event, db_event)
+    event.talks&.each do |talk|
+      db_talk = Talk.find_or_create_by!(
+        **talk.slice(:title, :description, :video_url),
+        event: db_event,
+        speakers: talk.speakers.map(&:name).join
+      )
+      db_talk.save
+    end
+  end
+
+  def build_db_event(event)
+    DatabaseEvent.new(
+      **event.slice(:date, :description, :name, :slug),
+      event_type: event.type,
+      registration_url: event.registration_link,
+      venue: build_db_venue(event),
+      **DATABASE_EVENT_DEFAULTS
+    )
+  end
+
+  def import_yaml_events
+    Melbourne::Event.all.each do |event| # rubocop:disable Rails/FindEach
+      if DatabaseEvent.find_by(name: event.name)
+        Rails.logger.info "Event #{event.name} already exists - skipped"
+      else
+        db_event = build_db_event(event)
+        if db_event.save
+          build_db_talks(event, db_event)
+          Rails.logger.info "Imported Event: #{event.name}"
+        else
+          Rails.logger.error "Failed to import event #{event.name}: #{db_event.errors.full_messages.to_sentence}"
+        end
+      end
+    end
+
+    def set_seeds_meetup_admin
+      if (user = User.find_by_email("meetup_admin@example.com"))
+        user.meetup_admin = true
+        if user.save
+          Rails.logger.info "Set user #{user.email} to have meetup_admin rights"
+        else
+          Rails.logger.error "Failed to update user #{user.email} to have meetup_admin rights: #{user.errors.full_messages.to_sentence}"
+        end
+      end
     end
   end
 end
