34bb7017cd3d0294271976c6bb52c1d1c9366844
Lee Sheppard <lee@leesheppard.com>
2026-06-05 14:21:37 +1000

    Resolved rubocop issues
    Helper issue:
    - Method has too many lines.
    - Assignment Branch Condition size for substitute_content is too high.

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -9,16 +9,28 @@ module CampaignsHelper
   def substitute_content(content, rsvp)
     return content if rsvp.nil?
 
+    content = substitute_member_vars(content, rsvp)
+    content = substitute_event_vars(content, rsvp)
+    substitute_rsvp_links(content, rsvp)
+  end
+
+  private
+
+  def substitute_member_vars(content, rsvp)
     content = content.gsub('{{member.name}}', rsvp.membership.full_name)
-    content = content.gsub('{{unsubscribe_link}}', unsubscribe_invitation_url(rsvp.token))
+    content.gsub('{{unsubscribe_link}}', unsubscribe_invitation_url(rsvp.token))
+  end
 
-    if rsvp.rsvp_event
-      content = content.gsub('{{event.title}}', rsvp.rsvp_event.title)
-      content = content.gsub('{{event.date}}', rsvp.rsvp_event.happens_at.strftime('%B %d, %Y'))
-    end
+  def substitute_event_vars(content, rsvp)
+    return content unless rsvp.rsvp_event
 
+    content = content.gsub('{{event.title}}', rsvp.rsvp_event.title)
+    content.gsub('{{event.date}}', rsvp.rsvp_event.happens_at.strftime('%B %d, %Y'))
+  end
+
+  def substitute_rsvp_links(content, rsvp)
     content.gsub(
-      '{{ rsvp_links }}',
+      '{{rsvp_links}}',
       tag.p(
         "#{link_to 'Yes', confirm_rsvp_url(rsvp.token)} " \
         "#{link_to 'No', decline_rsvp_url(rsvp.token)}".html_safe,
