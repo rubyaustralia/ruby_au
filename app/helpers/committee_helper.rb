7feeea5669b74183c0fa69ca82c7cdf09449f507
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2026-07-11 17:28:55 +1000

    refactor presidents func

--- app/helpers/committee_helper.rb
+++ app/helpers/committee_helper.rb
@@ -4,30 +4,42 @@ module CommitteeHelper
   end
 
   def presidents
-    current_committee = YAML.load_file Rails.root.join('config/data/committee.yml')
-    previous_committee = YAML.load_file Rails.root.join('config/data/previous.yml')
+    president_list = load_presidents
 
-    president_list = (current_committee + previous_committee)
-                     .filter { |member| president?(member) }
-                     .map { |p| { name: p['name'], start: to_array(p['start']), end: to_array(p['end']) } }
-
-    grouped = []
-    president_list.each do |record|
-      multiple = grouped.find { |item| item[:name] == record[:name] }
-
-      if multiple
-        multiple[:start].concat(record[:start]).sort!
-        multiple[:end].concat(record[:end]).sort!
-        president_list.delete(record)
-      else
-        grouped << record
-      end
-    end
+    group_presidents(president_list)
 
     president_list.sort! { |a, b| b[:start][-1] <=> a[:start][-1] }
   end
 
   private
+
+  def load_presidents
+    current_committee = YAML.load_file Rails.root.join('config/data/committee.yml')
+    previous_committee = YAML.load_file Rails.root.join('config/data/previous.yml')
+
+    (current_committee + previous_committee)
+      .filter { |member| president?(member) }
+      .map { |p| { name: p['name'], start: to_array(p['start']), end: to_array(p['end']) } }
+  end
+
+  def group_presidents(president_list)
+    grouped = []
+    president_list.each do |record|
+      other = grouped.find { |item| item[:name] == record[:name] }
+
+      if other
+        combine_records(other, record)
+        president_list.delete(record)
+      else
+        grouped << record
+      end
+    end
+  end
+
+  def combine_records(other, record)
+    other[:start].concat(record[:start]).sort!
+    other[:end].concat(record[:end]).sort!
+  end
 
   def president?(member)
     member['position'].downcase == 'president' ||
