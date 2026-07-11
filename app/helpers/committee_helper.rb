3025a4f7114ee4cb8582b0e3ffc542eaf3724c17
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2026-07-11 17:10:29 +1000

    sort presidents latest to oldest

--- app/helpers/committee_helper.rb
+++ app/helpers/committee_helper.rb
@@ -7,9 +7,24 @@ module CommitteeHelper
     current_committee = YAML.load_file Rails.root.join('config/data/committee.yml')
     previous_committee = YAML.load_file Rails.root.join('config/data/previous.yml')
 
-    (current_committee + previous_committee)
-      .filter { |member| president?(member) }
-      .map { |p| { name: p['name'], start: to_array(p['start']), end: to_array(p['end']) } }
+    president_list = (current_committee + previous_committee)
+                     .filter { |member| president?(member) }
+                     .map { |p| { name: p['name'], start: to_array(p['start']), end: to_array(p['end']) } }
+
+    grouped = []
+    president_list.each do |record|
+      multiple = grouped.find { |item| item[:name] == record[:name] }
+
+      if multiple
+        multiple[:start].concat(record[:start]).sort!
+        multiple[:end].concat(record[:end]).sort!
+        president_list.delete(record)
+      else
+        grouped << record
+      end
+    end
+
+    president_list.sort! { |a, b| b[:start][-1] <=> a[:start][-1] }
   end
 
   private
