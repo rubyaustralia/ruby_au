572d18b7ec70450e709e4c83fcf8c0a3705618b4
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2026-07-11 11:34:12 +1000

    refactor presidents func

--- app/helpers/committee_helper.rb
+++ app/helpers/committee_helper.rb
@@ -8,7 +8,18 @@ module CommitteeHelper
     previous_committee = YAML.load_file Rails.root.join('config/data/previous.yml')
 
     (current_committee + previous_committee)
-      .filter { |member| member['position'].downcase == 'president' || member['position'].downcase.split(',').any? { |post| post.gsub(/\([^)]*\)/, '').strip == 'president' } }
-      .map { |p| { name: p['name'], start: p['start'].is_a?(Array) ? p['start'] : [p['start']], end: p['end'].is_a?(Array) ? p['end'] : [p['end']] } }
+      .filter { |member| president?(member) }
+      .map { |p| { name: p['name'], start: to_array(p['start']), end: to_array(p['end']) } }
+  end
+
+  private
+
+  def president?(member)
+    member['position'].downcase == 'president' ||
+      member['position'].downcase.split(',').any? { |position| position.gsub(/\([^)]*\)/, '').strip == 'president' }
+  end
+
+  def to_array(term)
+    term.is_a?(Array) ? term : [term]
   end
 end
