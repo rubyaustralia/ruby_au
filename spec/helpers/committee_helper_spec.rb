38c194bf5f8712355a1a4d7d8197ead41a6c125b
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2026-07-23 12:43:50 +1000

    improve test for presidents timeline

--- spec/helpers/committee_helper_spec.rb
+++ spec/helpers/committee_helper_spec.rb
@@ -1,17 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe CommitteeHelper, type: :helper do
+  before do
+    allow(YAML).to receive(:load_file).with(Rails.root.join('config/data/committee.yml')).and_return([{ 'name' => 'Melbourne Blue', 'position' => 'President', 'start' => %w[2026-01-01 2020-03-03], 'end' => %w[2026-12-31 2020-03-02] }])
+    allow(YAML).to receive(:load_file).with(Rails.root.join('config/data/previous.yml')).and_return([{ 'name' => 'Ruby Red', 'position' => 'president', 'start' => %w[2001-01-01 2002-03-03], 'end' => %w[2001-12-31 2003-03-02] },
+                                                                                                     { 'name' => 'Wild Boar', 'position' => 'president', 'start' => %w[2000-01-01 2005-03-03], 'end' => %w[2000-12-31 2006-03-02] }, { 'name' => 'Pig Pink', 'position' => 'president', 'start' => %w[2010-01-01 2011-03-03], 'end' => %w[2010-12-31 2012-03-02] }])
+  end
+
   describe '#presidents' do
-    it 'returns a list of presidents' do
-      expect(helper.presidents).not_to be_empty
-    end
+    it 'returns a list of presidents and terms sorted by the latest start date to the oldest start date' do
+      presidents = helper.presidents
 
-    it 'has Keith as the oldest president' do
-      expect(helper.presidents[-1][:name]).to eq('Keith Pitty')
-    end
+      expect(presidents).not_to be_empty
 
-    it 'is 2012/06/16 when the second oldest president term starts' do
-      expect(helper.presidents[-2][:start][0]).to eq('2012-06-16')
+      names = presidents.map { |p| p[:name] }
+      expect(names).to eq(['Melbourne Blue', 'Pig Pink', 'Wild Boar', 'Ruby Red'])
+
+      start_dates = presidents.map { |p| p[:start] }
+      expect(start_dates).to eq([
+                                  %w[2026-01-01 2020-03-03],
+                                  %w[2010-01-01 2011-03-03],
+                                  %w[2000-01-01 2005-03-03],
+                                  %w[2001-01-01 2002-03-03]
+                                ])
+
+      end_dates = presidents.map { |p| p[:end] }
+      expect(end_dates).to eq([
+                                %w[2026-12-31 2020-03-02],
+                                %w[2010-12-31 2012-03-02],
+                                %w[2000-12-31 2006-03-02],
+                                %w[2001-12-31 2003-03-02]
+                              ])
     end
   end
 end
